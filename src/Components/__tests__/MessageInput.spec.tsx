32c5414d8d08755bb7640a3e3ac82c87ee717c9d
SuperBesse <julien.besse@gsoi.fr>
2022-11-22 23:46:20 +0100

    [Message]: integrate test for component and redux

---
+++ src/Components/__tests__/MessageInput.spec.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { act, render, screen } from '@testing-library/react-native';
+import MessageInput from '../MessageInput';
+import * as redux from 'react-redux';
+import { TextInput } from 'react-native';
+import { put, takeEvery } from 'redux-saga/effects';
+import addMessageSaga, { addMessage } from '@/Redux/Sagas/AddMessage';
+import { expectSaga } from 'redux-saga-test-plan';
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+
+jest.mock('../../Translations');
+const useDispatchSpy = jest.spyOn(redux, 'useDispatch');
+afterAll(() => useDispatchSpy.mockClear());
+
+describe('MessageInput', () => {
+  let generator = null;
+  beforeAll(() => {
+    generator = addMessageSaga();
+  });
+  it('should rencer correctly', () => {
+    const tree = render(<MessageInput />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should action send message on onEndEditing event', async () => {
+    render(<MessageInput />).toJSON();
+
+    const addMessagePayload = {
+      payload: { message: '' },
+      type: 'SEND_MESSAGE_START',
+    };
+    act(() => {
+      screen.UNSAFE_getByType(TextInput).props.onEndEditing();
+    });
+    expect(mockDispatch).toHaveBeenCalledWith(addMessagePayload);
+    useDispatchSpy.mockClear();
+  });
+
+  it('should trigger message saga when message action is send', async () => {
+    put({ type: 'SEND_MESSAGE_START' });
+    const actual = generator.next();
+    expect(actual.value).toEqual(takeEvery('SEND_MESSAGE_START', addMessage));
+
+    // return expectSaga(addMessageSaga)
+    //   .put({
+    //     type: 'SEND_MESSAGE_SUCCESS',
+    //     message: 'message test',
+    //   })
+    //   .dispatch({
+    //     type: 'SEND_MESSAGE_SUCCESS',
+    //     payload: { message: 'message test' },
+    //   })
+    //   .run();
+  });
+});
--- src/Redux/Sagas/AddMessage.ts
+++ src/Redux/Sagas/AddMessage.ts
@@ -6,23 +6,36 @@ import {
   SEND_MESSAGE_FAILURE,
 } from '@/Redux/Actions/MessageActions';
 
-export default function* addPostSaga() {
+export default function* addMessageSaga() {
+  console.log('addMessageSaga');
   yield takeEvery(SEND_MESSAGE_START, addMessage);
 }
 
-const delay = (time: number) =>
-  new Promise(resolve => setTimeout(resolve, time));
+const randomTime = () => Math.floor(Math.random() * 4000);
+
+const delay = () => new Promise(resolve => setTimeout(resolve, randomTime()));
+
+const randomError = (): Boolean => Math.floor(Math.random() * 100) < 30;
 
 function* addMessage(action: ActionType<string>) {
+  console.log('addMessage');
   console.log(action);
-  yield call(delay, 2000);
+  yield call(delay);
   try {
+    const error: Boolean = yield call(randomError);
+    if (error) {
+      throw new Error('fake error');
+    }
+    console.log('SEND_MESSAGE_SUCCESS');
     yield put({
       type: SEND_MESSAGE_SUCCESS,
       payload: { message: String, date: Date.now() },
     });
   } catch (err) {
     console.log(err);
+    console.log('SEND_MESSAGE_FAILURE');
     yield put({ type: SEND_MESSAGE_FAILURE, payload: {} });
   }
 }
+
+export { addMessage };
